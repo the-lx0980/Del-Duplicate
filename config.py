9e0637b1f63683a13447d9605ab81cae854635cd
Protullen <138602811+protullen@users.noreply.github.com>
2023-10-04 14:13:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,16 +15,13 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = 6353248
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = '1346f958b9d917f0961f3e935329eeee'
 
     # Authorized users to use this bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-
-    # session name
-    TG_USER_SESSION_NAME = os.environ.get("TG_USER_SESSION_NAME", "oxmohsen")
 
     # tg user session string
     TG_USER_SESSION_STRING = os.environ.get("TG_USER_SESSION_STRING", "")
