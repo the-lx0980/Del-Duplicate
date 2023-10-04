a0347862787be16baad814d7e27f6cd467e5493c
Protullen <138602811+protullen@users.noreply.github.com>
2023-10-04 14:43:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Duplicate Delete Bot'
+    return 'Duplicate-Delete-Bot'
 if __name__ == "__main__":
     app.run()
